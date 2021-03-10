7385d6809ac78f9e57e63f4492c8d68b2d7541f6
Zac Alexander <zac@znacreative.com>
2021-03-10 21:52:34 +0000

    WordSearch pair programming

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -3,6 +3,7 @@ const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
+// Not working
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
@@ -20,6 +21,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
+  // Working
   it("should return true if the word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -34,5 +36,27 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present and vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'P', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'A', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'I', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'R', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'D', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'V', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'PAIRDEV')
+
+    assert.isTrue(result);
+  });
+
+  it("should return an empty array if letters is empty", function() {
+    const result = wordSearch([], [])
+
+    assert.deepEqual(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return undefined;
+  }
+  const newArray = [];
+  for (let i = 0; i < letters.length; i++) {
+    newArray.push([]);
+    for (let j = 0; j <= letters[i].length; j++) {
+      newArray[i].push(letters[j][i]);
     }
-}
+  }
 
-module.exports = wordSearch
+  const verticalJoin = newArray.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word));
+    return true;
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+wordSearch(letters);
+module.exports = wordSearch;
